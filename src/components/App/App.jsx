1ecb7b2dd103e11dd831acde7040c16bbdfd8755
Alexander Ruziska <ziskapants@gmail.com>
2025-02-13 15:57:25 -0600

    more setup

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,7 +7,7 @@ function App() {
   
 
   // useEffect(() => {
-  //   // store."Do we need a refresh?"();
+  //   store."Do we need a refresh?"();
   // }, []);
 
   // const handleRefresh = () => {
@@ -17,19 +17,18 @@ function App() {
     <div>
       <h1>Giphy Search!</h1>
       <div className="image-item">
-        {store?.gifs.map(gif => (
-          <div key={gif.id} className="card">
-            <div className="front">
-              <img src={gif.images.original.url} alt="Image" />
-              <button type="button" className="btn btn-primary">&#9829;</button>
-            </div>
-          </div>
-        ))}
+    <div className='card'>
+      
+      <div className="front"> <img src="gif.images.original.url" alt="Image" /> 
+        <button type="button" className="btn btn-primary"> Favorite This! &#9829; </button>
       </div>
+      </div>
+      
+    </div>
     </div>
   );
-  
 }
+
 
 export default App;
 
