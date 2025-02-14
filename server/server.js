eb71ba8496f048620cb049a0dbc2f0a9a6876efa
Alecia Hagen <alecia.h98@gmail.com>
2025-02-13 18:22:07 -0600

    added the api search function to the backend

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const favoriteRouter = require('./routes/favorite.router');
+// const favoriteRouter = require('./routes/favorite.router');
 const categoryRouter = require('./routes/category.router');
+const searchRouter = require('./routes/search.router');
 const PORT = process.env.PORT || 5001;
 
 /** ---------- MIDDLEWARE ---------- **/
@@ -11,8 +12,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
-app.use('/api/favorites', favoriteRouter);
+// app.use('/api/favorites', favoriteRouter);
 app.use('/api/categories', categoryRouter);
+app.use('/api/search', searchRouter);
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
