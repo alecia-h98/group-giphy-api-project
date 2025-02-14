c2d44641d0693968acddaf99957d24b6047c6bf9
Alecia Hagen <alecia.h98@gmail.com>
2025-02-14 14:25:03 -0600

    added naviation to the Favorites list and App/Menu

--- src/components/App/FavoritesList/FavoritesList.jsx
+++ src/components/App/FavoritesList/FavoritesList.jsx
@@ -1,11 +1,17 @@
 import React from "react"
+import { useNavigate } from "react-router-dom";
 
 function FavoritesList() {
-    
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    alert("You are headed to the homepage");
+    navigate('/');
+  }; 
 
   return (
     <div>
-      
+         <button onClick={() => handleClick()}>Home</button>
     </div>
   )
 };
